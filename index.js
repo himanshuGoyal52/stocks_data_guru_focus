8106a004fb63bbb10f765604e3892874624f01d4
Himanshu Goyal <goyalhimanshu424@gmail.com>
2024-11-08 17:10:01 +0530

    after premium bought edits from office laptop

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const ExcelJS = require("exceljs");
 const mongoose = require('mongoose');
+const fs = require('node:fs');
 const {fundamentalData} = require("./models/fundamentalData.js");
 require('dotenv').config()
+
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 var exchange = "NSE"; // {NSE , BOM}
 var login_url = "https://www.gurufocus.com/login/";
 var data_url = "https://www.gurufocus.com/term/";
 var fundamentals_parameter = [ 
-    "mktcap",
-    "pe-ratio",
-    "operating-margin",
-    "roe",
-    "roce",
-    "debt-to-equity",
-    "pb-ratio",
-    "peg-ratio",
-    "rvn-growth-3y",
-    "ebitda-growth-3y",
-    "cashflow-growth-3y",
-    "interest-coverage",
-    "current-ratio",
-    "fscore"
+    // "mktcap",
+    "pe-ratio"
+    // "operating-margin",
+    // "roe",
+    // "roce",
+    // "debt-to-equity",
+    // "pb-ratio",
+    // "peg-ratio",
+    // "rvn-growth-3y",
+    // "ebitda-growth-3y",
+    // "cashflow-growth-3y",
+    // "interest-coverage",
+    // "current-ratio",
+    // "fscore"
 ];
 
 const connectMongo = async () => {
@@ -74,43 +79,107 @@ const scrapeFundamentalData = async (username , password) => {
 
     await page.goto(login_url);
 
-    await page.waitForSelector("[id=login-dialog-name-input]" , {visible : true});
+    await page.waitForSelector(`[id="login-dialog-name-input"]` , {visible : true});
 
     // login form //
     await page.screenshot({path: "./images/1_login_page.png"});
 
-    await page.type('[id=login-dialog-name-input]' , username);
-    await page.type('[id=login-dialog-pass-input]' , password);
+    await page.type(`[id="login-dialog-name-input"]` , username);
+    await page.type(`[id="login-dialog-pass-input"]` , password);
 
-    await page.screenshot({path : "./images/2_login_page_filled.png"})
+    await page.screenshot({path : "./images/2_login_page_filled.png"});
 
     await page.click('[type=button]');
     
     // await page.waitForNetworkIdle();
+    await page.waitForNavigation();
     // await page.waitForSelector("div[id=el-popover-975]" , {visible : true});
 
-    let stocks_symbols = await getStocksSymbols();
+    //let stocks_symbols = await getStocksSymbols();
+    let stocks_symbols = ["NSE:TATAMOTORS"]
 
     // Premium data
     for(let i=0 ; i<1 ; i++){
         for(let j=0 ; j<fundamentals_parameter.length ; j++){
-            let url = data_url + fundamentals_parameter[j] + "/" + stocks_symbols[i];
-            await page.goto(url);
+            //let url = data_url + fundamentals_parameter[j] + "/" + stocks_symbols[i];
+            let url = "https://www.gurufocus.com/modules/chart/term/term_chart.php?symbol=NSE:TATAMOTORS&series[]=pe"
+            await page.goto(url , { waitUntil: 'networkidle2' });
+
+            // await page.waitForNetworkIdle();
+
+            // await page.waitForSelector('[id="median"]' , {visible : true});
+
+            // await page.click('[id="median"]');
+
+            // await page.click("g ::-p-text(All)");
+
+
+            // const targetUrlPart = '/interactive_chart_json_morn.php'; // Replace with the unique part of the XHR request URL
+
+        
             
-            await page.waitForSelector("h1" , {visible : true});
+            // await page.waitForSelector("h1" , {visible : true});
 
-            await page.screenshot({path : "./images/" + fundamentals_parameter[j] + ".png"})
+            //await page.screenshot({path : "./images/" + fundamentals_parameter[j] + ".png"})
 
-            const bodyHandle = await page.$('body');
-            const page_headings = await page.evaluate((body) => {
-                // native broswer //
-                return body.innerHTML;
-            } , bodyHandle);
-            await bodyHandle.dispose();
+            const bodyHandle = await page.$$('script');
+            let aaruoa = [];
+            for(let z=0 ; z < bodyHandle.length ; z++){
+                const page_headings = await page.evaluate((body) => {
+                    // native broswer //
+                    return body.innerHTML;
+                } , bodyHandle[z]);
+    
+                await bodyHandle[z].dispose();
 
-            console.log("===================================================");
-            console.log(page_headings);
-            console.log("===================================================");
+                // console.log(page_headings);
+                if(page_headings.includes("NSE:TATAMOTORS") && page_headings.includes("data :")){
+                    aaruoa.push(page_headings);
+                }
+            }
+
+
+            //console.log(aaruoa[0]);
+            //console.log(aaruoa[1]);
+            
+            let data_split_arr = aaruoa[0].split("data :");
+            let final_arr = [];
+            
+
+            for(let z=0 ; z<data_split_arr.length ; z++){
+                if(data_split_arr[z].includes("[[")){
+                    final_arr.push(data_split_arr[z]);
+                }
+            }
+
+            let two_d_array = final_arr[0];
+
+            let end_idx = two_d_array.indexOf("]]");
+            let finally_mill_gaya = JSON.parse(
+                two_d_array.substring(0 , end_idx+2)
+            );
+
+            console.log(finally_mill_gaya);
+
+
+            // console.log("===================================================");
+            
+            // fs.writeFile("./zero.html" , aaruoa[0] , err => {
+            //     if(err){
+            //         console.log(err);
+            //     }else{
+            //         console.log("Files write success!");
+            //     }
+            // })
+
+            // fs.writeFile("./first.html" , aaruoa[1] , err => {
+            //     if(err){
+            //         console.log(err);
+            //     }else{
+            //         console.log("Files write success!");
+            //     }
+            // })
+            // console.log("===================================================");
 
             // console.log(page_headings);
         }
